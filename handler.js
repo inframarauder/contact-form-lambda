60a5adac238fa38a83f9e65cecf3bbfceb00296b
Subhasis Das <subhasisdas125@gmail.com>
2020-06-13 00:30:35 +0530

    initial nodemailer setup

--- handler.js
+++ handler.js
@@ -1,8 +1,39 @@
 "use strict";
+require("dotenv");
+const nodemailer = require("nodemailer");
+
+const transport = nodemailer.createTransport({
+  host: "smtp.gmail.com",
+  port: 587,
+  auth: {
+    user: process.env.EMAIL,
+    pass: process.env.PASSWORD,
+  },
+
+  tls: {
+    rejectUnauthorized: false,
+  },
+});
 
 module.exports.staticSiteMailer = async (event) => {
-  return {
-    statusCode: 200,
-    body: JSON.stringify(event.body, null, 2),
-  };
+  try {
+    const mailOptions = {
+      from: event.body.email,
+      to: "subhasisdas125@gmail.com",
+      subject: event.body.subject,
+      body: event.body.message,
+    };
+    await transport.sendMail(mailOptions);
+    return {
+      statusCode: 200,
+    };
+  } catch (error) {
+    console.error(error);
+    return {
+      statusCode: 500,
+      body: {
+        error: error.message,
+      },
+    };
+  }
 };
